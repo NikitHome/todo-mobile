3e663e6f2de5cbe14d94e4ca0b066c42b3afd9d8
Nikita Khomenko <mishanikita261202@mail.ru>
2023-10-19 13:12:46 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/pages/active_tasks.dart';
-import 'package:todo_app/pages/start.dart';
+import 'package:todo_app/pages/active_tasks_page/active_tasks_page.dart';
+import 'package:todo_app/pages/complete_tasks_page/complete_tasks_page.dart';
+import 'package:todo_app/pages/start_page/start_page.dart';
 
-void main() {
+
+void main() async {
   runApp(MaterialApp(
     theme: ThemeData(
         primaryColor: const Color.fromRGBO(24, 78, 119, 1.0)
@@ -10,7 +12,8 @@ void main() {
     initialRoute: '/',
     routes: {
       '/': (context) => const StartScreen(),
-      '/todo': (context) => const ActiveTasks()
+      '/active_tasks': (context) => const ActiveTasks(),
+      '/complete_tasks': (context) => const CompleteTasks()
     },
   ));
 }
